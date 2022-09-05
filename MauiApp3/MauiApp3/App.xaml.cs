c0ac4cf3ba22cabf73c91c1ca105cfc2381c9ec9
gm391 <remy.maitre@gmail.com>
2022-09-05 22:15:12 +0200

    Add code

---
+++ MauiApp3/MauiApp3/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace MauiApp3;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+        CultureInfo.DefaultThreadCurrentCulture = CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("fr-FR");
+        MainPage = new MainPage();
+	}
+}
---
+++ MauiApp3/MauiApp3/MauiProgram.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+using MauiApp3.Data;
+using System.Globalization;
+
+namespace MauiApp3;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiApp<App>()
+			.ConfigureFonts(fonts =>
+			{
+				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+			});
+        CultureInfo.DefaultThreadCurrentCulture = CultureInfo.DefaultThreadCurrentUICulture = new CultureInfo("fr");
+        builder.Services.AddMauiBlazorWebView();
+		#if DEBUG
+		builder.Services.AddBlazorWebViewDeveloperTools();
+#endif
+		
+		builder.Services.AddSingleton<WeatherForecastService>();
+
+		return builder.Build();
+	}
+}
